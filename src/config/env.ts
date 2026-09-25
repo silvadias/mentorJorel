1c3a416c177560dc42634346dd47e1b20b223e97
Luis Carlos da Silva Dias <silvadias.perfil@outlook.com>
2026-09-24 23:35:52 -0300

    feat: install gemini api services

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 const env = {
   port: Number(process.env['PORT']) || 3000,
   nodeEnv: process.env['NODE_ENV'] || 'development',
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
 };
 
 export default env;
